9394600d0373ab53a074d8117e01b713f50b4216
adrian <sun-moon1814@hotmail.com>
2024-03-18 20:37:56 -0600

    cambio

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var mysql = require('mysql');
-
-var conexion = mysql.createConnection({
+import mysql from "mysql";
+const connection = mysql.createConnection({
     host: 'localhost',
     database: 'alecrmjafm',
     user: 'root',
     password: ''
 });
 
-conexion.connect(function(error){
-    if(error){
-        throw error;
-    }else{
-        console.log('conexion establecida');
-    }
-})
-conexion.end();
+export default connection;
